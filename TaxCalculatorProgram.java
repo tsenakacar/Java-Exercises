06875143b5b7575da236475028ea60269763b57e
Sena <tsenakacar@gmail.com>
2022-08-26 17:40:37 +0300

    Update TaxCalculatorProgram.java

--- TaxCalculatorProgram.java
+++ TaxCalculatorProgram.java
@@ -3,13 +3,22 @@ import java.util.Scanner;
 public class TaxCalculatorProgram {
 
     public static void main(String[] args) {
-        double tax =1.8;
-        Scanner input= new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         System.out.println("Tax-free price of the product");
-        double value=input.nextDouble();
-        System.out.println("Taxed price of the product");
-        double taxedprice= ((value*tax)/10)+value;
-        System.out.println(taxedprice);
+        double value = input.nextDouble();
+
+        if (value < 1000 && value > 0) {
+            double tax = 1.8;
+            System.out.println("Taxed price of the product");
+            double taxedprice = ((value * tax) / 10) + value;
+            System.out.println(taxedprice);
+        } else if (value > 1000) {
+
+            double tax = 0.8;
+            System.out.println("Taxed price of the product");
+            double taxedprice = ((value * tax) / 10) + value;
+            System.out.println(taxedprice);
+        }
     }
-    
+
 }
